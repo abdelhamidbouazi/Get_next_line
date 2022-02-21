90c7dba6bc21583a7a6494a16e993acb8c662a0e
Abdelhamid Bouazi <abouazi@e1r7p4.1337.ma>
2022-02-21 21:21:19 +0100

    abdelhamid

---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouazi <abouazi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/14 21:31:24 by abouazi           #+#    #+#             */
+/*   Updated: 2022/02/15 03:26:02 by abouazi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(const char *s1);
+char	*ft_read(char *leftxt, int fd);
+char	*ft_get_line(char	*leftxt);
+char	*ft_stock(char *leftxt);
+char	*get_next_line(int fd);
+
+#endif
